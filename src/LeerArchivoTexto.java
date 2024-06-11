6ccfe9a1641c1a1e99eed984cc73bf2c3e8b4400
Emilio <elprofesoremilio@gmail.com>
2024-06-11 10:17:25 +0200

    Clase

--- src/LeerArchivoTexto.java
+++ src/LeerArchivoTexto.java
@@ -1,3 +1,44 @@
-public class LeerArchivoTexto {
+import java.io.*;
 
+public class LeerArchivoTexto {
+    public static void main(String[] args) {
+        String nombreArchivo = "prueba.txt";
+        // BufferedReader --> FileReader --> File
+
+        File file = new File(nombreArchivo);
+        FileReader fr = null;
+        BufferedReader br = null;
+        System.out.println("-------- Inicio de archivo --------");
+
+        try {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            String linea;
+
+            while ((linea=br.readLine())!=null) {
+                System.out.println(linea);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error al abrir lectura de archivo");
+        } catch (IOException e) {
+            System.out.println("Error de lectura");
+        } finally {
+
+            try {
+                if (br!=null) {
+                    br.close();
+                }
+                if (fr!=null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error al cerrar archivo");
+            }
+        }
+
+        System.out.println("---------- Fin de archivo ----------");
+
+
+    }
 }
